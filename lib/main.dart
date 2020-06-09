4f0876419511de81e5fdb6f7ac64324f7dfd6b7b
Emamezi <emamezi.ebebeinwe@stu.cu.edu.ng>
2020-06-09 15:12:03 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'PrimeRoase Mobile',
         theme: ThemeData(
             appBarTheme: AppBarTheme(
-          color: Colors.blue,
+          color: Colors.brown,
         )),
         home: PlaceListView(),
         routes: {
